751996e3850df436c3045d53272fe8f57768230e
helpfulrobot <chris@silverstripe.com>
2016-01-01 05:20:59 +1300

    Converted to PSR-2

--- code/MetaKeywordsExtension.php
+++ code/MetaKeywordsExtension.php
@@ -1,28 +1,31 @@
 <?php
 
-class MetaKeywordsExtension extends DataExtension {
+class MetaKeywordsExtension extends DataExtension
+{
 
-	private static $db = array(
-		"MetaKeywords" => "Varchar(1024)",
-	);
+    private static $db = array(
+        "MetaKeywords" => "Varchar(1024)",
+    );
 
-	public function updateCMSFields(FieldList $fields) {
-		$metaData = $fields->fieldByName('Root.Main.Metadata');
+    public function updateCMSFields(FieldList $fields)
+    {
+        $metaData = $fields->fieldByName('Root.Main.Metadata');
 
-		$metaFieldTitle = new TextareaField("MetaKeywords", $this->owner->fieldLabel('MetaKeywords'));
-		$metaData->insertAfter($metaFieldTitle, 'MetaDescription');
+        $metaFieldTitle = new TextareaField("MetaKeywords", $this->owner->fieldLabel('MetaKeywords'));
+        $metaData->insertAfter($metaFieldTitle, 'MetaDescription');
 
-		return $fields;
-	}
+        return $fields;
+    }
 
-	public function updateFieldLabels(&$labels) {
-		$labels['MetaKeywords'] = _t('SiteTree.METAKEYWORDS', "Meta Keywords");
-	}
+    public function updateFieldLabels(&$labels)
+    {
+        $labels['MetaKeywords'] = _t('SiteTree.METAKEYWORDS', "Meta Keywords");
+    }
 
-	public function MetaTags(&$tags) {
-		if($this->owner->MetaKeywords) {
-			$tags .= "<meta name=\"keywords\" content=\"" . Convert::raw2att($this->owner->MetaKeywords) . "\" />\n";
-		}
-	}
-
+    public function MetaTags(&$tags)
+    {
+        if ($this->owner->MetaKeywords) {
+            $tags .= "<meta name=\"keywords\" content=\"" . Convert::raw2att($this->owner->MetaKeywords) . "\" />\n";
+        }
+    }
 }
